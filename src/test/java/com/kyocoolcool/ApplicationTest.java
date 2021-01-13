3a71542b96a2982d7eb99d858f334e845c9ff878
Chris Chen <kyocoolcool@hotmail.com>
2021-01-13 17:49:31 +0800

    test5

--- src/test/java/com/kyocoolcool/ApplicationTest.java
+++ src/test/java/com/kyocoolcool/ApplicationTest.java
@@ -8,6 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ApplicationTest {
     @Test
      void test() {
-        Assertions.assertEquals( 1 + 2 , 3);
+        Assertions.assertEquals( 2 + 2 , 4);
     }
 }
